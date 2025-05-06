707398bb3b663ce1772b38bbff164fb9602f34d6
Leonardo Gabriel <Leonardogabrielx@gmail.com>
2025-05-06 16:35:04 -0700

    Initial Commit

---
+++ DeathbyDiceTests/DeathbyDiceTests.swift
@@ -0,0 +1,17 @@
+//
+//  DeathbyDiceTests.swift
+//  DeathbyDiceTests
+//
+//  Created by Leonardo Gabriel on 5/6/25.
+//
+
+import Testing
+@testable import DeathbyDice
+
+struct DeathbyDiceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
